5470c9460e4c6183648b8b9ab5de4f38505ae33c
Michał - MiB - Bachowski <michal@bachowski.pl>
2011-03-02 18:22:36 +0100

    Removed old test file

--- lib_2_1/test.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from authentication import Authentication
-
-if __name__ == "__main__":
-    a = Authentication(32578741,'michal@bachowski.pl','j6fgxE7WoJ')
-    print a.isValid()
